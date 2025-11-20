9aa77c073ad44ea9028c2dc31808f3a1300232d9
a01736943-design <a01736943@tec.mx>
2025-11-19 18:14:56 -0600

    j

--- forvia/utils/data_loader.py
+++ forvia/utils/data_loader.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 @st.cache_data
 def load_percentage_data():
-    return pd.read_csv('/Users/hijos/Desktop/Analitica Prof/Prof. Rigoberto/M2_Streamlit/Forvia/data/percentage_not_completed.csv')
+    return pd.read_csv('forvia/data/percentage_not_completed.csv')
 
 @st.cache_data
 def load_projects_data():
-    df = pd.read_csv('/Users/hijos/Desktop/Analitica Prof/Prof. Rigoberto/M2_Streamlit/Forvia/data/projects.csv', encoding='latin1')
+    df = pd.read_csv('forvia/data/projects.csv', encoding='latin1')
     df = df.iloc[:-2].copy()
     df['Percent complete']=pd.to_numeric(df['Percent complete'], errors='coerce')
     return df
